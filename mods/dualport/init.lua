a9c0834650dd162915862bd20b8d007326798457
bas080 <bas080@hotmail.com>
2013-03-18 04:30:48 +0100

    Fixed infotext property and added linked sound effect

--- mods/dualport/init.lua
+++ mods/dualport/init.lua
@@ -38,31 +38,40 @@ end
 
 teleporters.link = function(pos)
   local save_contents
-  print("check if file exists")
+  --print("check if file exists")
   local f = io.open(teleporters.file, "r")
   if f then
-    print("file exists")
+    --print("file exists")
     local contents = f:read()
     io.close(f)
     if contents == "" or contents == nil then
-      print("nothing to pair with saving coordinates")
+      --print("nothing to pair with saving coordinates")
       save_contents = tostring(pos.x..","..pos.y..","..pos.z)
-      print(save_contents)
+      --print(save_contents)
     else
-      print("linking with uneven node")
+      --print("linking with uneven node")
       local p = {}
       p.x, p.y, p.z = string.match(contents, "^([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+)$")
       if p.x and p.y and p.z then
         coordinates = {x = tonumber(p.x),y= tonumber(p.y),z = tonumber(p.z)}
+        --get node names
+        local meta = minetest.env:get_meta(coordinates)
+        local name_two = meta:get_string("name")
+        local meta = minetest.env:get_meta(pos)
+        local name_one = meta:get_string("name")
+        --set node infotext and link
         local meta = minetest.env:get_meta(coordinates)
         meta:set_string("link", pos.x..","..pos.y..","..pos.z)
+        meta:set_string("infotext", name_two.." > "..name_one)
+        --notify
+        minetest.sound_play("dualport_linked",{pos=pos,gain=0.7,max_hear_distance=32})
+        minetest.chat_send_all("Dualport linked to "..meta:get_string("infotext"))
         meta:set_string("infotext", meta:get_string("infotext"))
         
-        minetest.chat_send_all("[TELE] Linked to "..meta:get_string("infotext"))
-        
+        local name = meta:get_string("name")
         local meta = minetest.env:get_meta(pos)
         meta:set_string("link", contents)
-        meta:set_string("infotext", meta:get_string("infotext"))
+        meta:set_string("infotext", name_one.." > "..name_two)
         save_contents = ""
         
       else
@@ -73,7 +82,7 @@ teleporters.link = function(pos)
     f:write(save_contents)
     io.close(f)
   else
-    print("file does not exist")
+    --print("file does not exist")
     save_contents = tostring(pos.x..","..pos.y..","..pos.z)
     local f = io.open(teleporters.file, "w")
     f:write(save_contents)
@@ -105,7 +114,7 @@ minetest.register_node("dualport:dualport", {
       local objs = minetest.env:get_objects_inside_radius(pos, .5)
       pos.y = pos.y -.5
       for _, obj in pairs(objs) do
-        minetest.sound_play("teleporters_teleport",{pos=pos,gain=0.4,max_hear_distance=32})
+        minetest.sound_play("dualport_teleport",{pos=pos,gain=0.4,max_hear_distance=32})
         newpos.y = newpos.y -1
         teleporters.teleport({obj=obj,pos=newpos})
         newpos.y = newpos.y +1.7
@@ -117,9 +126,9 @@ minetest.register_node("dualport:dualport", {
   after_place_node = function(pos, placer)
     local meta = minetest.env:get_meta(pos)
     local player = placer:get_player_name()
-    meta:set_string("infotext", "Unknown")
+    meta:set_string("infotext", "unnamed")
     meta:set_string("link", "")
-    meta:set_string("name", "")
+    meta:set_string("name", "unnamed")
     meta:set_string("owner", player)
     meta:set_string("formspec",teleporters.formspec(meta))
     teleporters.link(pos)
@@ -135,11 +144,12 @@ minetest.register_node("dualport:dualport", {
       local meta = minetest.env:get_meta(p)
       local name = meta:get_string("name")
       
-      if name == "" then
-        meta:set_string("infotext","Unknown > "..fields.desc)
+      --print("link:"..link)
+      
+      if link == "" then
         meta = minetest.env:get_meta(pos)
         meta:set_string("name",fields.desc)
-        meta:set_string("infotext", fields.desc.." > Unknown")
+        meta:set_string("infotext", fields.desc)
       else
         meta:set_string("infotext", meta:get_string("name").." > "..fields.desc)
         name = meta:get_string("name")
