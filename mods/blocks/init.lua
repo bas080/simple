28868d61433a035921a64847c479db19b9eb7a24
bas080 <bas080@hotmail.com>
2013-04-11 11:00:16 +0200

    Fixed the chandelier

--- mods/blocks/init.lua
+++ mods/blocks/init.lua
@@ -2,14 +2,13 @@ minetest.register_alias("blocks:stone_bricks", "default:stonebrick")
 
 minetest.register_node("blocks:chandelier_hanger", {
   description = "Chandelier",
+  tiles = {"blocks_no_texture.png"},
   sunlight_propagates = true,
-  groups = {hanging_node=1},
-  walkable = false;
+  groups = {choppy=2,oddly_breakable_by_hand=3, hanging_node=1},
+  drawtype = "plantlike",
+  walkable = false,
   pointable = false,
-  selection_box = {
-    type = "fixed",
-    fixed = {0, 0, 0, 0, 0, 0},
-  },
+  drop = {},
 })
 
 minetest.register_node("blocks:chandelier", {
@@ -28,19 +27,21 @@ minetest.register_node("blocks:chandelier", {
   after_place_node = function(pos,node)
     local a = minetest.env:get_node({x=pos.x, y=pos.y-1, z=pos.z})
     if a.name == "air" then
-      minetest.env:remove_node(pos)
       minetest.env:set_node({x=pos.x, y=pos.y, z=pos.z}, {name="blocks:chandelier_hanger"})
       minetest.env:set_node({x=pos.x, y=pos.y-1, z=pos.z}, {name="blocks:chandelier"})
     else
       drop_attached_node(pos)
     end
   end,
+  after_dig_node = function(pos,node)
+    minetest.env:remove_node({x=pos.x, y=pos.y+1, z=pos.z})
+  end
 })
 
 minetest.register_node("blocks:chain_steel", {
-  description = "Bronze Chain",
+  description = "Bronze Steel",
   drawtype = "plantlike",
-  tiles = {"blocks_chain_bronze.png"},
+  tiles = {"blocks_chain_steel.png"},
   visual_scale = 1,
   paramtype = 'light',
   walkable = false;
@@ -58,7 +59,7 @@ minetest.register_node("blocks:chain_bronze", {
   tiles = {"blocks_chain_bronze.png"},
   visual_scale = 1,
   paramtype = 'light',
-  walkable = true;
+  walkable = false;
   sunlight_propagates = true,
   groups = {choppy=2,oddly_breakable_by_hand=3, hanging_node=1},
   selection_box = {
