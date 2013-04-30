7b5d0038ccd8406321c04ab61623fc559953b024
bas080 <bas080@hotmail.com>
2013-04-30 06:17:42 +0200

    Added more jungletree aliases

--- mods/trees/aliases.lua
+++ mods/trees/aliases.lua
@@ -6,6 +6,12 @@ minetest.register_alias("moretrees:fir_trunk", "trees:tree_conifer")
 minetest.register_alias("moretrees:palm_trunk", "trees:tree_palm")
 minetest.register_alias("moretrees:palm_leaves", "trees:leaves_palm")
 
+minetest.register_alias("jungletree:leaves_green", "trees:leaves_green")
+minetest.register_alias("jungletree:leaves_red", "trees:leaves_red")
+minetest.register_alias("jungletree:leaves_yellow", "trees:leaves_yellow")
+
+
+
 minetest.register_alias("moretrees:jungletree_trunk", "default_jungletree")
 minetest.register_alias("moretrees:jungletree_sapling", "trees:jungletree_sapling")
 minetest.register_alias("moretrees:jungletree_leaves_green", "trees:leaves_green")
