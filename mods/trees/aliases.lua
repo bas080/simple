2efce95bc4849af6f21b6dac80ad4b5bbe3b6134
bas080 <bas080@hotmail.com>
2013-04-30 03:03:00 +0200

    Fixed aliases

--- mods/trees/aliases.lua
+++ mods/trees/aliases.lua
@@ -1,4 +1,4 @@
 minetest.register_alias("moretrees:jungletree_sapling", "trees:jungletree_sapling")
-minetest.register_alias("moretrees:leaves_green", "trees:leaves_green")
-minetest.register_alias("moretrees:leaves_yellow", "trees:leaves_yellow")
-minetest.register_alias("moretrees:leaves_red", "trees:leaves_red")
+minetest.register_alias("moretrees:jungletree_leaves_green", "trees:leaves_green")
+minetest.register_alias("moretrees:jungletree_leaves_yellow", "trees:leaves_yellow")
+minetest.register_alias("moretrees:jungletree_leaves_red", "trees:leaves_red")
