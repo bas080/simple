e8e20433df548961889af19dba1da58721a8daf3
bas080 <bas080@hotmail.com>
2013-04-30 03:00:09 +0200

    Removed moretrees mod and added many changes to trees mod

---
+++ mods/trees/aliases.lua
@@ -0,0 +1,4 @@
+minetest.register_alias("moretrees:jungletree_sapling", "trees:jungletree_sapling")
+minetest.register_alias("moretrees:leaves_green", "trees:leaves_green")
+minetest.register_alias("moretrees:leaves_yellow", "trees:leaves_yellow")
+minetest.register_alias("moretrees:leaves_red", "trees:leaves_red")
--- mods/trees/init.lua
+++ mods/trees/init.lua
@@ -3,6 +3,7 @@ abstract_trees = {}
 dofile(minetest.get_modpath("trees").."/jungletree.lua")
 dofile(minetest.get_modpath("trees").."/mangrovetree.lua")
 dofile(minetest.get_modpath("trees").."/palmtree.lua")
+dofile(minetest.get_modpath("trees").."/loaftree.lua")
 --dofile(minetest.get_modpath("trees").."/aliases.lua")
 --dofile(minetest.get_modpath("trees").."/conifertree.lua")
 
@@ -13,7 +14,7 @@ minetest.register_tool("trees:drawer", {
 	on_use = function(itemstack, user, pointed_thing)
 		if pointed_thing.under then
 			local height = 4+math.ceil(math.random(11))
-			abstract_trees.grow_jungletree(pointed_thing.above, height)
+			abstract_trees.grow_mangrovetree(pointed_thing.above, height)
 		end
 	end,
 })
